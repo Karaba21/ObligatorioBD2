0abba802271411daf503df6c9c01ee9a3c9f8fba
lukibenitez <Lucas.benitez@correo.ucu.edu.uy>
2025-06-18 20:19:25 -0300

    Cambios en el login y otros arreglos

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 // import HomePage from './Components/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
