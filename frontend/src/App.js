daa2a5119208174d2a0ac675c67e6aad76732002
lukibenitez <Lucas.benitez@correo.ucu.edu.uy>
2025-06-18 18:41:29 -0300

    Arreglo main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './Components/LoginPage';
-<<<<<<< HEAD
 import RegisterPage from './Components/RegisterPage';
+import VotarPage from './Components/VotarPage';
+import VotoExitoPage from './Components/VotoExitoPage';
 
 function App() {
     return (
@@ -10,24 +11,11 @@ function App() {
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
+                <Route path="/votar" element={<VotarPage />} />
+                <Route path="/voto-exito" element={<VotoExitoPage />} />
             </Routes>
         </Router>
     );
-=======
-import VotarPage from './Components/VotarPage';
-import VotoExitoPage from './Components/VotoExitoPage';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/votar" element={<VotarPage />} />
-        <Route path="/voto-exito" element={<VotoExitoPage />} />
-      </Routes>
-    </Router>
-  );
->>>>>>> main
 }
 
 export default App; 
