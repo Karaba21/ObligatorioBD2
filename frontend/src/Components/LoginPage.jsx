bb0366cc90f3eba242521a2edee356f0f8f03cf6
lukibenitez <Lucas.benitez@correo.ucu.edu.uy>
2025-06-18 20:51:15 -0300

    Arreglo fragmento duplicado en LoginPage

--- frontend/src/Components/LoginPage.jsx
+++ frontend/src/Components/LoginPage.jsx
@@ -20,16 +20,6 @@ const LoginMiembroMesa = () => {
         }
 
         try {
-
-            const response = await fetch(
-                "http://localhost:5001/api/login",
-                {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ ci, password }),
-                }
-            );
-
             const response = await fetch("http://localhost:5001/api/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
